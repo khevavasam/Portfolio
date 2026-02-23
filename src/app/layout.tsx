ad78f216fa3fdc6aa9b0ec6cc42f1a8b019808ef
khevavasam <khevavasam@gmail.com>
2026-02-23 23:15:17 +0300

    Remove Bootstrap from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -10,8 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      {/* темная тема бустрапа */}
-      <body data-bs-theme="dark">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
