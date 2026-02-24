68fea6a9a76724f0d01cf864d0134edf64d3fa8d
khevavasam <khevavasam@gmail.com>
2026-02-24 16:40:45 +0300

    Disable image optimization for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactCompiler: true, 
-    output: "export",
-};
-export default nextConfig;
+    reactCompiler: true,
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  export default nextConfig
+  
